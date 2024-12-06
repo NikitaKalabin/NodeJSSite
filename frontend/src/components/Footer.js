5cc3a4dea63088ae1758dbf410bc57b122f0de0d
Nikita <115951587+NikitaKalabin@users.noreply.github.com>
2024-12-06 04:05:28 +0300

    add state usage

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,26 +1,58 @@
-import React, { useContext } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { ThemeContext } from "../context/ThemeContext";
 
-const Footer = ({ text }) => {
-  const { theme } = useContext(ThemeContext);
+class Footer extends Component {
+  static contextType = ThemeContext;
 
-  const footerStyles = {
-    padding: "10px 20px",
-    textAlign: "center",
-    backgroundColor: theme === "light" ? "#f0f0f0" : "#333",
-    color: theme === "light" ? "#000" : "#fff",
-    position: "relative",
-    bottom: 0,
-    width: "100%",
+  constructor(props) {
+    super(props);
+    this.state = {
+      footerText: props.text,
+    };
+  }
+
+  handleChangeText = () => {
+    this.setState({
+      footerText: "Visit my GitHub: https://github.com/bober20",
+    });
   };
 
-  return (
-    <footer style={footerStyles}>
-      <p>{text}</p>
-    </footer>
-  );
-};
+  render() {
+    const { theme } = this.context;
+    const { footerText } = this.state;
+
+    const footerStyles = {
+      padding: "10px 20px",
+      textAlign: "center",
+      backgroundColor: theme === "light" ? "#f0f0f0" : "#333",
+      color: theme === "light" ? "#000" : "#fff",
+      position: "relative",
+      bottom: 0,
+      width: "100%",
+    };
+
+    const buttonStyles = {
+      padding: "5px 10px",
+      borderRadius: "5px",
+      border: "none",
+      cursor: "pointer",
+      backgroundColor:
+        theme === "light" ? "rgb(0, 150, 136)" : "rgb(0, 128, 115)",
+      color: "#fff",
+      marginTop: "10px",
+    };
+
+    return (
+      <footer style={footerStyles}>
+        <p>{footerText}</p>
+        <button style={buttonStyles} onClick={this.handleChangeText}>
+          Change Text
+        </button>
+      </footer>
+    );
+  }
+}
 
 Footer.propTypes = {
   text: PropTypes.string,
